065b7e7cd9a70418f5bab40cd0ba864e6f7f68e5
kishoremp2007 <kishore948740@gmail.com>
2025-10-28 14:22:08 +0530

    Add files via upload

---
+++ Def maximum.py
@@ -0,0 +1,8 @@
+def maximum(a,b):
+    if a>b:
+        print("Maximum:", a)
+    else:
+        print("maximum:", b)
+a=int(input("Enter the 1st number:"))
+b=int(input("Enter the 2nd number:"))
+maximum(a,b)
